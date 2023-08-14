ac0411ea20f8860e994857f798eae6ea7fa5ffcf
Kalinga <kalingaru48@gamil.com>
2023-08-14 17:53:15 +0530

    feat: first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using MigrationsDemo;
+
+
+var db = new BlogContext();
+
+if (!db.Posts.Any())
+{
+    Console.WriteLine("Adding sample data");
+    db.AddRange(
+        new Post { Title = "Intro to Migrations" },
+        new Post { Title = "Migrations in Team Environments" },
+        new Post { Title = "Advanced Migration" });
+    db.SaveChanges();
+}
+
+Console.WriteLine("Posts:");
+foreach (var post in db.Posts)
+{
+    Console.WriteLine($" {post.Title}");
+}
---
+++ ConsoleApp2/Model.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MigrationsDemo;
+
+class BlogContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+      => options.UseNpgsql("Host=localhost;Database=blog;Username=postgres;Password=example");
+}
+
+class Post
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
